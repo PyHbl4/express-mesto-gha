04451d4303993a368132cfd1de8689b38d0435e5
PyHbl4 <maxim_runkov@mail.ru>
2023-07-08 18:40:02 +0500

    feat: реализованы роуты добавления/получения пользователей

---
+++ app.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error(err));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64a965224849158cb9ecfb8a'
+  };
+
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', require('./routes/users'));
+
+app.listen(PORT);
